eb71640bedcd76ee8acc84d2a04b8893680cc178
jhansi vaddineni <jhansi.developer123@gmail.com>
2018-01-15 02:01:49 -0500

    Latest Updated Code forCurrency Calculator

--- CurrencyConvertor.js
+++ CurrencyConvertor.js
@@ -4,25 +4,27 @@ function convertor(i) {
     var baseCurrencyText = baseCurrency.options[baseCurrency.selectedIndex].text;
     var targetCurrency = document.getElementsByName("target_currency")[i];
     var targetCurrencyText = targetCurrency.options[targetCurrency.selectedIndex].text;
-    
-    var url = "https://api.fixer.io/latest";
-    var params = "base="+baseCurrencyText+"&symbols="+targetCurrencyText;
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        if(baseCurrencyText != targetCurrencyText) {
-            var obj = JSON.parse(this.responseText);
-            document.getElementsByName('convertor')[i].value = obj.rates[targetCurrencyText] * document.getElementsByName('convert')[i].value;
-        } else {
-            document.getElementsByName('convertor')[i].value = document.getElementsByName('convert')[i].value;
-        }
-        if(Math.sign(document.getElementsByName('convert')[i].value) == -1) {
-            document.getElementsByName('convertor')[i].value = "";
+    //Constructed API call in such a way that, When we send a value with the base currency we get the converted value in the target currency 
+    var params = "base=" + baseCurrencyText + "&symbols=" + targetCurrencyText;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (baseCurrencyText != targetCurrencyText) {
+                //Parsing response JSON
+                var obj = JSON.parse(this.responseText);
+                document.getElementsByName('convertor')[i].value = obj.rates[targetCurrencyText] * document.getElementsByName('convert')[i].value;
+            } else {
+                document.getElementsByName('convertor')[i].value = document.getElementsByName('convert')[i].value;
             }
-    } 
-    else if(this.readyState == 4 && this.status != 200){
-        alert("OOPS! Something Went Wrong");
+            //Handling Negative Values Input
+            if (Math.sign(document.getElementsByName('convert')[i].value) == -1) {
+                document.getElementsByName('convertor')[i].value = "";
+            }
+        }
+        //API Outage
+        else if (this.readyState == 4 && this.status != 200) {
+            alert("OOPS! Something Went Wrong");
+        }
     }
-    }
-    xhttp.open("GET", url+"?"+params, true);
+    xhttp.open("GET", API_URL + "?" + params, true);
     xhttp.send();
 }
